b8fbc53f67ca4ceb504d38f74b8c6e423a6f94ff
TahirUddinAhmed <01tahirahmed@gmail.com>
2025-08-07 11:41:11 +0530

    Generate Fake data using Fectory(), work on the EventController to fetch, all record, single record and also store an event in the database

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -12,7 +13,12 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        $events = Event::latest()->get();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $events
+        ]);
     }
 
     /**
@@ -20,15 +26,28 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:start_time'
+        ]);
+
+
+        $event = Event::create([
+            ...$data,
+            'user_id' => 1
+        ]);
+
+        return $event;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        return $event;
     }
 
     /**
