e4741dbae6810bc4805b44755479789fedd7af67
TahirUddinAhmed <01tahirahmed@gmail.com>
2025-08-19 01:06:01 +0530

    Feat: sign out - revoking user token and Scheduling a command to run every day

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class ,'login']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
 
 // public route 
 Route::apiResource('events', EventController::class)
