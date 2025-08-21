7276f805c5e85aef3afc6cda7adc0669210024de
TahirUddinAhmed <01tahirahmed@gmail.com>
2025-08-21 12:38:35 +0530

    create a custom artisan commnand to notify atendees about the event that is going to start in 24 hours.

---
+++ app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends notifications to all event attendees that event starts soon';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = Event::with('attendees.user')
+                    ->whereBetween('start_time', [now(), now()->addDay()])
+                    ->get();
+        $eventCount = $events->count();
+        $eventLabel = Str::plural('event', $eventCount);
+
+        /**
+         * Todo: 
+         *      -> iterate over all the $events and notify all the attendees.user that 
+         *          the event is going to start in next 24 hours
+         */ 
+        $events->each(
+            fn($event) => $event->attendees->each(
+            fn($attendee) => $this->info("Notifying the user {$attendee->user->name}")) 
+        );
+
+        $this->info("Found {$eventCount} {$eventLabel}");
+        $this->info("Here are the events - ");
+        foreach($events as $event) {
+            $this->info($event->name);
+        }
+    }
+}
